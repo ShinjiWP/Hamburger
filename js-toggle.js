9806ffad9cc11b994f1f16386e5e5e3ed69110de
ShinjiWP <xkounagox@gmail.com>
2021-11-16 23:12:20 +0900

    202111162312

---
+++ js-toggle.js
@@ -0,0 +1,7 @@
+$(function () {
+	$(".p-header__box").on("click", function () {
+        // $(".c-shadow").toggleClass("p-shadow");
+        $(".c-grid--sidebar").css({ "transform": "translate(-100%, 0)", "transition": "2s" });
+	});
+});
+		
