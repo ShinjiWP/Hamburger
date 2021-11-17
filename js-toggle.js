969080fb6a824cde896695098ac3060b7f42cd0f
ShinjiWP <xkounagox@gmail.com>
2021-11-18 08:56:26 +0900

    202111180900

--- js-toggle.js
+++ js-toggle.js
@@ -37,15 +37,42 @@
 // });
 
 
-$(function () {
-	$(".p-header__box").on("click", function () {
-		$(".c-shadow").fadeIn(600);
-		// $("body").css("position", "fixed");
-		$(".p-sidebar").css({ left: "-100%", transition: "0.6s" });
-	});
+// $(function () {
+// 	$(".p-header__box").on("click", function () {
+// 		$(".c-shadow").fadeIn(600);
+// 		// $("body").css("position", "fixed");
+// 		$(".p-sidebar").css({ left: "-100%", transition: "0.6s" });
+// 	});
 
+// 	$(".p-sidebar__x").on("click", function () {
+// 		$(".c-shadow").fadeOut(500);
+// 		$(".p-sidebar").css("left", "100%");
+// 	});
+// });
+const mediaQuery = window.matchMedia("(max-width: 599.9px)");
+
+// ページが読み込まれた時に実行
+handle(mediaQuery);
+
+// ウィンドウサイズを変更しても実行（ブレイクポイントの監視）
+mediaQuery.addListener(handle);
+
+function handle(mm) {
+	if (mm.matches) {
+		// ウィンドウサイズ768px以下のときの処理
+		$(".p-header__box").on("click", function () {
+			$(".c-shadow").fadeIn(500);
+			$(".p-sidebar").css({ left: "14%", transition: "0.5s" });
+		});
+	} else {
+		// それ以外の処理
+		$(".p-header__box").on("click", function () {
+			$(".c-shadow").fadeIn(500);
+			$(".p-sidebar").css({ left: "57%", transition: "0.5s" });
+		});
+	}
 	$(".p-sidebar__x").on("click", function () {
 		$(".c-shadow").fadeOut(500);
-		$(".p-sidebar").css("left", "100%");
+		$(".p-sidebar").css({ left: "100%", transition: "0.5s" });
 	});
-});
+}
