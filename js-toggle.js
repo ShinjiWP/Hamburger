2e05d33dd365464ab4ab7a6e5e3fa9e911a7bec3
ShinjiWP <xkounagox@gmail.com>
2021-11-17 14:05:24 +0900

    202111171400

--- js-toggle.js
+++ js-toggle.js
@@ -1,9 +1,22 @@
 $(function () {
-	$(".p-header__box").on("click", function () {
-		$(".c-shadow").fadeIn(500);
-		// $("body").css("position", "fixed");
-		$(".c-grid--sidebar").css({ transform: "translate(-86%, 0)", transition: "1s" });
-	});
+	if (window.matchMedia("(max-width:599.9px)").matches) {
+		$(".p-header__box").on("click", function () {
+			$(".c-shadow").fadeIn(500);
+			// $("body").css("position", "fixed");
+			$(".c-grid--sidebar").css({ transform: "translate(-86%, 0)", transition: "1s" });
+		});
+	} else {
+		$(".p-header__box").on("click", function () {
+			$(".c-shadow").fadeIn(500);
+			// $("body").css("position", "fixed");
+			$(".c-grid--sidebar").css({ transform: "translate(-43%, 0)", transition: "1s" });
+		});
+	}
+	// $(".p-header__box").on("click", function () {
+	// 	$(".c-shadow").fadeIn(500);
+	// 	// $("body").css("position", "fixed");
+	// 	$(".c-grid--sidebar").css({ transform: "translate(-86%, 0)", transition: "1s" });
+	// });
 	$(".p-sidebar__x").on("click", function () {
 		$(".c-shadow").fadeOut(500);
 		$(".c-grid--sidebar").css({ transform: "translate(0%, 0)", transition: "1s" });
