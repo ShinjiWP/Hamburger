1707068649205b7dad0b285f83675ec8294555bc
ShinjiWP <xkounagox@gmail.com>
2021-11-17 18:44:49 +0900

    202111171845

--- js-toggle.js
+++ js-toggle.js
@@ -1,24 +1,37 @@
+// $(function () {
+// 	if (window.matchMedia("(max-width:599.9px)").matches) {
+// 		$(".p-header__box").on("click", function () {
+// 			$(".c-shadow").fadeIn(600);
+// 			// $("body").css("position", "fixed");
+// 			$(".c-grid--sidebar").css({ transform: "translate(-86%, 0)", transition: "1s" });
+// 		});
+// 	} else if (window.matchMedia("(mix-width:600px)").matches) {
+// 		$(".p-header__box").on("click", function () {
+// 			$(".c-shadow").fadeIn(600);
+// 			// $("body").css("position", "fixed");
+// 			$(".c-grid--sidebar").css({ transform: "translate(-43%, 0)", transition: "1s" });
+// 		});
+// 	}
+// 	// $(".p-header__box").on("click", function () {
+// 	// 	$(".c-shadow").fadeIn(500);
+// 	// 	// $("body").css("position", "fixed");
+// 	// 	$(".c-grid--sidebar").css({ transform: "translate(-86%, 0)", transition: "1s" });
+// 	// });
+// 	$(".p-sidebar__x").on("click", function () {
+// 		$(".c-shadow").fadeOut(500);
+// 		$(".c-grid--sidebar").css({ transform: "translate(0%, 0)", transition: "1s" });
+// 	});
+// });
+
 $(function () {
-	if (window.matchMedia("(max-width:599.9px)").matches) {
-		$(".p-header__box").on("click", function () {
-			$(".c-shadow").fadeIn(500);
-			// $("body").css("position", "fixed");
-			$(".c-grid--sidebar").css({ transform: "translate(-86%, 0)", transition: "1s" });
-		});
-	} else {
-		$(".p-header__box").on("click", function () {
-			$(".c-shadow").fadeIn(500);
-			// $("body").css("position", "fixed");
-			$(".c-grid--sidebar").css({ transform: "translate(-43%, 0)", transition: "1s" });
-		});
-	}
-	// $(".p-header__box").on("click", function () {
-	// 	$(".c-shadow").fadeIn(500);
-	// 	// $("body").css("position", "fixed");
-	// 	$(".c-grid--sidebar").css({ transform: "translate(-86%, 0)", transition: "1s" });
-	// });
+	$(".p-header__box").on("click", function () {
+		$(".c-shadow").fadeIn(600);
+		// $("body").css("position", "fixed");
+        $(".p-sidebar").css({ "left": "-100%" ,"transition":"0.6s"});
+	});
+
 	$(".p-sidebar__x").on("click", function () {
 		$(".c-shadow").fadeOut(500);
-		$(".c-grid--sidebar").css({ transform: "translate(0%, 0)", transition: "1s" });
+		$(".p-sidebar").css("left", "100%");
 	});
 });
