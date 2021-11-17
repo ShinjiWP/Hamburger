f552c04e18a8a9adfc6f424941dd1c6d38cf8f48
ShinjiWP <xkounagox@gmail.com>
2021-11-18 08:51:09 +0900

    202111180850-jQuery-ok

--- js-toggle.js
+++ js-toggle.js
@@ -1,15 +1,13 @@
 // $(function () {
 // 	if (window.matchMedia("(max-width:599.9px)").matches) {
 // 		$(".p-header__box").on("click", function () {
-// 			$(".c-shadow").fadeIn(600);
-// 			// $("body").css("position", "fixed");
-// 			$(".c-grid--sidebar").css({ transform: "translate(-86%, 0)", transition: "1s" });
+// 			$(".c-shadow").fadeIn(500);
+// 			$(".p-sidebar").css({ left: "14%", transition: "0.5s" });
 // 		});
 // 	} else if (window.matchMedia("(mix-width:600px)").matches) {
 // 		$(".p-header__box").on("click", function () {
-// 			$(".c-shadow").fadeIn(600);
-// 			// $("body").css("position", "fixed");
-// 			$(".c-grid--sidebar").css({ transform: "translate(-43%, 0)", transition: "1s" });
+// 			$(".c-shadow").fadeIn(500);
+// 			$(".p-sidebar").css({ left: "57%", transition: "0.5s" });
 // 		});
 // 	}
 // 	// $(".p-header__box").on("click", function () {
@@ -19,10 +17,20 @@
 // 	// });
 // 	$(".p-sidebar__x").on("click", function () {
 // 		$(".c-shadow").fadeOut(500);
-// 		$(".c-grid--sidebar").css({ transform: "translate(0%, 0)", transition: "1s" });
+// 		$(".p-sidebar").css({ left: "100%", transition: "0.5s" });
 // 	});
 // });
 
+// $(function () {
+//     $(".p-header__box").on("click", function () {
+//         $(".c-shadow").fadeIn(600);
+//         $(".p-sidebar").css({ left: "14%", transition: "0.6s" });
+//     });
+
+//     $(".p-sidebar__x").on("click", function () {
+//         $(".p-sidebar").css("left", "100%");
+//     });
+// });
 // $(function () {
 // 	$(".p-header__box").on("click", function () {
 // 		$(".c-shadow").fadeIn(600);
@@ -36,16 +44,30 @@
 // 	});
 // });
 
+const mediaQuery = window.matchMedia("(max-width: 599.9px)");
 
-$(function () {
-	$(".p-header__box").on("click", function () {
-		$(".c-shadow").fadeIn(600);
-		// $("body").css("position", "fixed");
-		$(".p-sidebar").css({ left: "-100%", transition: "0.6s" });
-	});
+// ページが読み込まれた時に実行
+handle(mediaQuery);
 
-	$(".p-sidebar__x").on("click", function () {
+// ウィンドウサイズを変更しても実行（ブレイクポイントの監視）
+mediaQuery.addListener(handle);
+
+function handle(mm) {
+	if (mm.matches) {
+		// ウィンドウサイズ768px以下のときの処理
+		$(".p-header__box").on("click", function () {
+			$(".c-shadow").fadeIn(500);
+			$(".p-sidebar").css({ left: "14%", transition: "0.5s" });
+		});
+	} else {
+		// それ以外の処理
+		$(".p-header__box").on("click", function () {
+			$(".c-shadow").fadeIn(500);
+			$(".p-sidebar").css({ left: "57%", transition: "0.5s" });
+		});
+    }
+    $(".p-sidebar__x").on("click", function () {
 		$(".c-shadow").fadeOut(500);
-		$(".p-sidebar").css("left", "100%");
+		$(".p-sidebar").css({ left: "100%", transition: "0.5s" });
 	});
-});
+}
