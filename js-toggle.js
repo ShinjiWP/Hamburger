5a62bb64c4a457731ac9ff8b57bad2117c6b1fe5
ShinjiWP <xkounagox@gmail.com>
2021-11-19 08:42:06 +0900

    202111190840

--- js-toggle.js
+++ js-toggle.js
@@ -44,7 +44,7 @@
 // 	});
 // });
 
-const mediaQuery = window.matchMedia("(max-width: 599.9px)");
+const mediaQuery = window.matchMedia("(max-width: 1079px)");
 
 // ページが読み込まれた時に実行
 handle(mediaQuery);
@@ -52,22 +52,40 @@ handle(mediaQuery);
 // ウィンドウサイズを変更しても実行（ブレイクポイントの監視）
 mediaQuery.addListener(handle);
 
+// function handle(mm) {
+// 	if (mm.matches) {
+// 		// ウィンドウサイズ768px以下のときの処理
+// 		$(".p-header__box").on("click", function () {
+// 			$(".c-shadow").fadeIn(500);
+// 			$(".p-sidebar").css({ left: "14%", transition: "0.5s" });
+// 		});
+// 	} else  {
+// 		// それ以外の処理
+// 		$(".p-header__box").on("click", function () {
+// 			$(".c-shadow").fadeIn(500);
+// 			$(".p-sidebar").css({ left: "57%", transition: "0.5s" });
+// 		});
+//     }
+//     $(".p-sidebar__x").on("click", function () {
+// 		$(".c-shadow").fadeOut(500);
+// 		$(".p-sidebar").css({ left: "100%", transition: "0.5s" });
+//     });
+    
+// }
 function handle(mm) {
 	if (mm.matches) {
 		// ウィンドウサイズ768px以下のときの処理
 		$(".p-header__box").on("click", function () {
 			$(".c-shadow").fadeIn(500);
-			$(".p-sidebar").css({ left: "14%", transition: "0.5s" });
+			$(".p-sidebar").css({ right: "100%", transition: "0.5s" });
 		});
-	} else  {
+	} else {
 		// それ以外の処理
-		$(".p-header__box").on("click", function () {
-			$(".c-shadow").fadeIn(500);
-			$(".p-sidebar").css({ left: "57%", transition: "0.5s" });
-		});
-    }
-    $(".p-sidebar__x").on("click", function () {
+			$(".c-shadow").fadeOut(00);
+			$(".p-sidebar").css({ right: "0%", transition: "0s" });
+	}
+	$(".p-sidebar__x").on("click", function () {
 		$(".c-shadow").fadeOut(500);
-		$(".p-sidebar").css({ left: "100%", transition: "0.5s" });
+		$(".p-sidebar").css({ right: "0%", transition: "0.5s" });
 	});
 }
